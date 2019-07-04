10a17a01de348e0053da63181fef4449e9f1bb7e
Mocuishle <645852220@qq.com>
2019-07-04 15:03:15 +0800

    change scripts

--- rdb_to_pika/install_rdb_tools.sh
+++ rdb_to_pika/install_rdb_tools.sh
@@ -4,11 +4,4 @@ cd ../third/redis-rdb-tools
 sudo python setup.py install
 cd ../../rdb_to_pika
 
-rdb_filename=dump.rdb
-if [ $# != 0 ]
-then
-rdb_filename=$1
-fi
-echo "${rdb_filename}"
-rdb -c protocol -f protocol_file  ${rdb_filename}
 
